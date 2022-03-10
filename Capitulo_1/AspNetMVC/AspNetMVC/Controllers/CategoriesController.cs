74d5447f8f56872cada8277f42b5baf7930405fc
Douglas Gioielli <douglasgioielli@gmail.com>
2022-03-10 08:41:42 -0300

    **** Implementação do Capítulo 01
    Aqui foi realizada a implementação do primeiro capítulo do livro: ASP.NET MVC5 da Casa do Código.
     - Tive apenas uma dificuldade na configuração do formulário da tela de Delete para o submit enviar um objeto com o Id correto.

---
+++ Capitulo_1/AspNetMVC/AspNetMVC/Controllers/CategoriesController.cs
@@ -0,0 +1,92 @@
+﻿using AspNetMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetMVC.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        #region Variables and Properties
+
+        private static IList<CategoryModel> categories = new List<CategoryModel>() {
+            new CategoryModel() { CategoryId = 1, Name = "Notebooks" },
+            new CategoryModel() { CategoryId = 2, Name = "Monitores" },
+            new CategoryModel() { CategoryId = 3, Name = "Impressoras" },
+            new CategoryModel() { CategoryId = 4, Name = "Mouses" },
+            new CategoryModel() { CategoryId = 5, Name = "Desktops" },
+        };
+
+        #endregion
+
+        #region Functions
+
+        // GET
+        public IActionResult Index()
+        {
+            return View(categories);
+        }
+
+        // GET: Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create (CategoryModel category)
+        {
+            var maxId = categories.Max(x => x.CategoryId);
+            category.CategoryId = maxId + 1;
+            categories.Add(category);
+            return RedirectToAction("Index");
+        }
+
+        // GET: Edit
+        public IActionResult Edit(long id)
+        {
+            var category = categories.Where(x => x.CategoryId == id).First();
+            return View(category);
+        }
+
+        // POST: Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(CategoryModel category)
+        {
+            categories.Remove(categories.Where(x => x.CategoryId == category.CategoryId).First());
+            categories.Add(category);
+            return RedirectToAction("Index");
+        }
+
+        // GET: Details
+        public IActionResult Details(long id)
+        {
+            var category = categories.Where(x => x.CategoryId == id).First();
+            return View(category);
+        }
+
+        // GET: Delete
+        public IActionResult Delete(long id)
+        {
+            var category = categories.Where(x => x.CategoryId == id).First();
+            return View(category);
+        }
+
+        // POST: Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(CategoryModel category)
+        {
+            categories.Remove(categories.Where(x => x.CategoryId == category.CategoryId).First());
+            return RedirectToAction("Index");
+        }
+
+
+        #endregion
+    }
+}
---
+++ Capitulo_1/AspNetMVC/AspNetMVC/Models/CategoryModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetMVC.Models
+{
+    public class CategoryModel
+    {
+        #region Variables and Properties
+
+        public long CategoryId { get; set; }
+        public string Name { get; set; }
+
+        #endregion
+
+        #region Functions
+
+        public override string ToString() => $"{CategoryId} >> {Name}";
+
+        #endregion
+    }
+}
